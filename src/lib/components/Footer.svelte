c929048130b0d5b9eb0641efe75448f18682f830
Azadeh <azad6026@gmail.com>
2022-11-01 03:35:47 +0000

    Simplified footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,19 +3,6 @@
 	import { siteAuthor } from '$lib/config'
 </script>
 
-<!-- <footer>
-	<MainNav />
-
-	<nav>
-		<ul>
-			<li>
-				<a href="/api/rss.xml" rel="external">RSS</a>
-			</li>
-			<li>
-				<a href="/">Home</a>
-			</li>
-		</ul>
-	</nav>
-
-	<p>&copy;{new Date().getFullYear()} {siteAuthor}</p>
-</footer> -->
+<footer>
+	<p>&copy;{new Date().getFullYear()} - Write ❤️ Code Time </p>
+</footer>
